7240596d8d13dd5383b7286f973b20950873513c
SeemaD220583 <69263129+SeemaD220583@users.noreply.github.com>
2020-08-14 16:48:52 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template,request
-import os,requests,pickle,jsonify,sklearn
+import requests,os,pickle,jsonify,sklearn
 import numpy as np
 
-
 app=Flask(__name__)
+
 
 current_path = os.getcwd()
 pickle_path = os.path.join(current_path, "assets", "loan_predict.pkl")
@@ -35,11 +35,39 @@ def predict():
         prediction=model.predict([[loan_amnt,term,int_rate,emp_length,home_ownership,annual_inc,purpose,addr_state,dti,delinq_2yrs,revol_util,total_acc,longest_credit_length,verification_status]])
         output=prediction[0]
         if output==0:
-            return render_template('index.html',prediction_text="It does not lead to Defaulter Customer")
+            return render_template('index.html',prediction_text="It leads to Good Customer")
         else:
-            return render_template('index.html',prediction_text="It leads to Defaulter Customer")
+            return render_template('index.html',prediction_text="It leads to Bad Customer")
     else:
         return render_template("index.html")
+
+
+
+@app.route("/api/predict",methods=["POST"])
+def predict_api():
+    if request.method=='POST':
+        data = request.get_json()
+        loan_amnt=int(data.form['loan_amnt'])
+        term=int(data.form['term'])
+        int_rate=float(data.form['int_rate'])
+        emp_length=float(data.form['emp_length'])
+        home_ownership=int(data.form['home_ownership'])
+        annual_inc=float(data.form['annual_inc'])
+        annual_inc=np.log(annual_inc)
+        purpose=int(data.form['purpose'])
+        addr_state=int(data.form['addr_state'])
+        dti=float(data.form['dti'])
+        delinq_2yrs=float(data.form['delinq_2yrs'])
+        revol_util=float(data.form['revol_util'])
+        total_acc=float(data.form['total_acc'])
+        longest_credit_length=float(data.form['longest_credit_length'])
+        verification_status=int(data.form['verification_status'])
+        prediction=model.predict([[loan_amnt,term,int_rate,emp_length,home_ownership,annual_inc,purpose,addr_state,dti,delinq_2yrs,revol_util,total_acc,longest_credit_length,verification_status]])
+        output=prediction[0]
+        if output==0:
+            return jsonify("It is not a loan Defaulter")
+        return jsonify("It is loan Defaulter")
+
 
 if __name__=="__main__":
     app.run(debug=True)
